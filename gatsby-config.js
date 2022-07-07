c8306cc59f5cb5ce0f9a7c9336f83405e52d0e51
yy-shin <yunyol.shin@devsisters.com>
2022-07-07 19:28:45 +0900

    section 5 finished

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,19 @@
 module.exports = {
   siteMetadata: {
-    title: `Tutorial`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: `My Tutorial Blog Site`,
+    // siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
+    "gatsby-plugin-mdx"
+  ],
 }
+
---
+++ src/pages/about.js
@@ -0,0 +1,21 @@
+// Step 1: Import React
+import * as React from 'react'
+import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
+
+// Step 2: Define your component
+const AboutPage = (props) => {
+  return (
+    <Layout pageTitle="About Me">
+        <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+        <StaticImage
+        alt="Clifford, a reddish-brown pitbull, dozing in a bean bag chair"
+        src="../images/puzzleworld.png"
+        />
+    </Layout>
+  )
+}
+
+
+// Step 3: Export your component
+export default AboutPage
